91ca6056621354482e318f7f20c6de2d032b2d08
Brad Chandler <bchandler@fbhl.com>
2022-02-06 13:44:05 -0600

    added testing todo

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+//TODO: provide testing library for each of these
 const tcGet = require("./lib/getFetch");
 const tcPost = require("./lib/postFetch");
 const tcPut = require("./lib/putFetch");
 const tcDelete = require("./lib/deleteFetch");
 
 module.exports = {
-  tcGet, 
-  tcPost, 
-  tcPut, 
-  tcDelete
-}
+  tcGet,
+  tcPost,
+  tcPut,
+  tcDelete,
+};
